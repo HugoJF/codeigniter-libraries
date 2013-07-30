f50131c970583af018ed3343618db012ca650f42
HugoJF <hugojellerferreira@hotmail.com>
2013-07-29 22:30:54 -0400

    main controller base

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -5,11 +5,14 @@
 		public function index()
 		{
 			$this->load->database();
-			$this->load-library('DBConfig');
-			$this->load-library('TableBuilder');
-			$this->load-library('Paginator');
+			$this->load->library('DBConfig');
+			$this->load->library('TableBuilder');
+			$this->load->library('Paginator');
 
-
-
+			echo '<pre>';
+			echo '<h1>DBConfig Tests:</h1>';
+			echo '<h1>TableBuilder Tests:</h1>';
+			echo '<h1>Paginator Tests:</h1>';
+			echo '</pre>';
 		}
 	}
