b73d662fea3c67ca1d8d38986739b76d289c3424
HugoJF <hugojellerferreira@hotmail.com>
2013-07-29 22:26:20 -0400

    main controller

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -2,15 +2,14 @@
 
 	class Main extends CI_Controller
 	{
-
-		public function __construct()
-		{
-			parent::construct();
-		}
-
-
 		public function index()
 		{
+			$this->load->database();
+			$this->load-library('DBConfig');
+			$this->load-library('TableBuilder');
+			$this->load-library('Paginator');
+
+
 
 		}
 	}
